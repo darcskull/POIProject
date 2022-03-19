9d6352547644a9713357565c3f3461a35686ef3e
darcskull <74406901+darcskull@users.noreply.github.com>
2022-03-19 10:16:38 +0200

    update form for questions

--- POIProject/FormQuiz.Designer.cs
+++ POIProject/FormQuiz.Designer.cs
@@ -36,34 +36,10 @@ namespace POIProject
             this.textBoxC = new System.Windows.Forms.TextBox();
             this.textBoxD = new System.Windows.Forms.TextBox();
             this.LabelA = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBoxAnswer1 = new System.Windows.Forms.TextBox();
-            this.textBoxTime1 = new System.Windows.Forms.TextBox();
-            this.textBoxAnswer2 = new System.Windows.Forms.TextBox();
-            this.textBoxTime2 = new System.Windows.Forms.TextBox();
-            this.textBoxTime3 = new System.Windows.Forms.TextBox();
-            this.textBoxAnswer3 = new System.Windows.Forms.TextBox();
-            this.textBoxAnswer4 = new System.Windows.Forms.TextBox();
-            this.textBoxTime4 = new System.Windows.Forms.TextBox();
-            this.textBoxAnswer5 = new System.Windows.Forms.TextBox();
-            this.textBoxTime5 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
-            this.label13 = new System.Windows.Forms.Label();
-            this.label14 = new System.Windows.Forms.Label();
-            this.label15 = new System.Windows.Forms.Label();
-            this.label16 = new System.Windows.Forms.Label();
-            this.label17 = new System.Windows.Forms.Label();
-            this.label18 = new System.Windows.Forms.Label();
+            this.textBoxAnswers = new System.Windows.Forms.TextBox();
             this.buttonNextQuestion = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -80,7 +56,7 @@ namespace POIProject
             // textBoxQuestion
             // 
             this.textBoxQuestion.Enabled = false;
-            this.textBoxQuestion.Location = new System.Drawing.Point(344, 177);
+            this.textBoxQuestion.Location = new System.Drawing.Point(507, 164);
             this.textBoxQuestion.Multiline = true;
             this.textBoxQuestion.Name = "textBoxQuestion";
             this.textBoxQuestion.Size = new System.Drawing.Size(495, 117);
@@ -89,7 +65,7 @@ namespace POIProject
             // textBoxA
             // 
             this.textBoxA.Enabled = false;
-            this.textBoxA.Location = new System.Drawing.Point(370, 330);
+            this.textBoxA.Location = new System.Drawing.Point(533, 316);
             this.textBoxA.Name = "textBoxA";
             this.textBoxA.Size = new System.Drawing.Size(469, 20);
             this.textBoxA.TabIndex = 2;
@@ -97,7 +73,7 @@ namespace POIProject
             // textBoxB
             // 
             this.textBoxB.Enabled = false;
-            this.textBoxB.Location = new System.Drawing.Point(370, 380);
+            this.textBoxB.Location = new System.Drawing.Point(533, 363);
             this.textBoxB.Name = "textBoxB";
             this.textBoxB.Size = new System.Drawing.Size(469, 20);
             this.textBoxB.TabIndex = 3;
@@ -105,7 +81,7 @@ namespace POIProject
             // textBoxC
             // 
             this.textBoxC.Enabled = false;
-            this.textBoxC.Location = new System.Drawing.Point(370, 422);
+            this.textBoxC.Location = new System.Drawing.Point(533, 404);
             this.textBoxC.Name = "textBoxC";
             this.textBoxC.Size = new System.Drawing.Size(469, 20);
             this.textBoxC.TabIndex = 4;
@@ -113,7 +89,7 @@ namespace POIProject
             // textBoxD
             // 
             this.textBoxD.Enabled = false;
-            this.textBoxD.Location = new System.Drawing.Point(370, 466);
+            this.textBoxD.Location = new System.Drawing.Point(533, 466);
             this.textBoxD.Name = "textBoxD";
             this.textBoxD.Size = new System.Drawing.Size(469, 20);
             this.textBoxD.TabIndex = 5;
@@ -121,25 +97,16 @@ namespace POIProject
             // LabelA
             // 
             this.LabelA.AutoSize = true;
-            this.LabelA.Location = new System.Drawing.Point(341, 333);
+            this.LabelA.Location = new System.Drawing.Point(504, 323);
             this.LabelA.Name = "LabelA";
             this.LabelA.Size = new System.Drawing.Size(14, 13);
             this.LabelA.TabIndex = 6;
             this.LabelA.Text = "A";
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 36);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 13);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Участник 1";
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(341, 473);
+            this.label2.Location = new System.Drawing.Point(504, 473);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(13, 13);
             this.label2.TabIndex = 8;
@@ -148,7 +115,7 @@ namespace POIProject
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(341, 429);
+            this.label3.Location = new System.Drawing.Point(504, 411);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(14, 13);
             this.label3.TabIndex = 9;
@@ -157,221 +124,24 @@ namespace POIProject
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(341, 383);
+            this.label4.Location = new System.Drawing.Point(504, 366);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(14, 13);
             this.label4.TabIndex = 10;
             this.label4.Text = "Б";
             // 
-            // textBoxAnswer1
+            // textBoxAnswers
             // 
-            this.textBoxAnswer1.Enabled = false;
-            this.textBoxAnswer1.Location = new System.Drawing.Point(87, 69);
-            this.textBoxAnswer1.Name = "textBoxAnswer1";
-            this.textBoxAnswer1.Size = new System.Drawing.Size(70, 20);
-            this.textBoxAnswer1.TabIndex = 11;
-            // 
-            // textBoxTime1
-            // 
-            this.textBoxTime1.Enabled = false;
-            this.textBoxTime1.Location = new System.Drawing.Point(87, 109);
-            this.textBoxTime1.Name = "textBoxTime1";
-            this.textBoxTime1.Size = new System.Drawing.Size(70, 20);
-            this.textBoxTime1.TabIndex = 12;
-            // 
-            // textBoxAnswer2
-            // 
-            this.textBoxAnswer2.Enabled = false;
-            this.textBoxAnswer2.Location = new System.Drawing.Point(87, 363);
-            this.textBoxAnswer2.Name = "textBoxAnswer2";
-            this.textBoxAnswer2.Size = new System.Drawing.Size(70, 20);
-            this.textBoxAnswer2.TabIndex = 13;
-            // 
-            // textBoxTime2
-            // 
-            this.textBoxTime2.Enabled = false;
-            this.textBoxTime2.Location = new System.Drawing.Point(87, 404);
-            this.textBoxTime2.Name = "textBoxTime2";
-            this.textBoxTime2.Size = new System.Drawing.Size(70, 20);
-            this.textBoxTime2.TabIndex = 14;
-            // 
-            // textBoxTime3
-            // 
-            this.textBoxTime3.Enabled = false;
-            this.textBoxTime3.Location = new System.Drawing.Point(617, 76);
-            this.textBoxTime3.Name = "textBoxTime3";
-            this.textBoxTime3.Size = new System.Drawing.Size(68, 20);
-            this.textBoxTime3.TabIndex = 15;
-            // 
-            // textBoxAnswer3
-            // 
-            this.textBoxAnswer3.Enabled = false;
-            this.textBoxAnswer3.Location = new System.Drawing.Point(483, 76);
-            this.textBoxAnswer3.Name = "textBoxAnswer3";
-            this.textBoxAnswer3.Size = new System.Drawing.Size(64, 20);
-            this.textBoxAnswer3.TabIndex = 16;
-            // 
-            // textBoxAnswer4
-            // 
-            this.textBoxAnswer4.Enabled = false;
-            this.textBoxAnswer4.Location = new System.Drawing.Point(940, 76);
-            this.textBoxAnswer4.Name = "textBoxAnswer4";
-            this.textBoxAnswer4.Size = new System.Drawing.Size(67, 20);
-            this.textBoxAnswer4.TabIndex = 17;
-            // 
-            // textBoxTime4
-            // 
-            this.textBoxTime4.Enabled = false;
-            this.textBoxTime4.Location = new System.Drawing.Point(940, 109);
-            this.textBoxTime4.Name = "textBoxTime4";
-            this.textBoxTime4.Size = new System.Drawing.Size(67, 20);
-            this.textBoxTime4.TabIndex = 18;
-            // 
-            // textBoxAnswer5
-            // 
-            this.textBoxAnswer5.Enabled = false;
-            this.textBoxAnswer5.Location = new System.Drawing.Point(940, 343);
-            this.textBoxAnswer5.Name = "textBoxAnswer5";
-            this.textBoxAnswer5.Size = new System.Drawing.Size(67, 20);
-            this.textBoxAnswer5.TabIndex = 19;
-            // 
-            // textBoxTime5
-            // 
-            this.textBoxTime5.Enabled = false;
-            this.textBoxTime5.Location = new System.Drawing.Point(940, 380);
-            this.textBoxTime5.Name = "textBoxTime5";
-            this.textBoxTime5.Size = new System.Drawing.Size(67, 20);
-            this.textBoxTime5.TabIndex = 20;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(22, 112);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(40, 13);
-            this.label5.TabIndex = 21;
-            this.label5.Text = "Време";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(22, 72);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(49, 13);
-            this.label6.TabIndex = 22;
-            this.label6.Text = "Отговор";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(22, 316);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(64, 13);
-            this.label7.TabIndex = 23;
-            this.label7.Text = "Участник 2";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(22, 366);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(49, 13);
-            this.label8.TabIndex = 24;
-            this.label8.Text = "Отговор";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(1026, 79);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(49, 13);
-            this.label9.TabIndex = 25;
-            this.label9.Text = "Отговор";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(1026, 350);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(49, 13);
-            this.label10.TabIndex = 26;
-            this.label10.Text = "Отговор";
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(417, 76);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(49, 13);
-            this.label11.TabIndex = 27;
-            this.label11.Text = "Отговор";
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(1026, 112);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(40, 13);
-            this.label12.TabIndex = 28;
-            this.label12.Text = "Време";
-            // 
-            // label13
-            // 
-            this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(1026, 383);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(40, 13);
-            this.label13.TabIndex = 29;
-            this.label13.Text = "Време";
-            // 
-            // label14
-            // 
-            this.label14.AutoSize = true;
-            this.label14.Location = new System.Drawing.Point(562, 75);
-            this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(40, 13);
-            this.label14.TabIndex = 30;
-            this.label14.Text = "Време";
-            // 
-            // label15
-            // 
-            this.label15.AutoSize = true;
-            this.label15.Location = new System.Drawing.Point(22, 407);
-            this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(40, 13);
-            this.label15.TabIndex = 31;
-            this.label15.Text = "Време";
-            // 
-            // label16
-            // 
-            this.label16.AutoSize = true;
-            this.label16.Location = new System.Drawing.Point(937, 316);
-            this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(64, 13);
-            this.label16.TabIndex = 32;
-            this.label16.Text = "Участник 5";
-            // 
-            // label17
-            // 
-            this.label17.AutoSize = true;
-            this.label17.Location = new System.Drawing.Point(943, 36);
-            this.label17.Name = "label17";
-            this.label17.Size = new System.Drawing.Size(64, 13);
-            this.label17.TabIndex = 33;
-            this.label17.Text = "Участник 4";
-            // 
-            // label18
-            // 
-            this.label18.AutoSize = true;
-            this.label18.Location = new System.Drawing.Point(417, 36);
-            this.label18.Name = "label18";
-            this.label18.Size = new System.Drawing.Size(64, 13);
-            this.label18.TabIndex = 34;
-            this.label18.Text = "Участник 3";
+            this.textBoxAnswers.Enabled = false;
+            this.textBoxAnswers.Location = new System.Drawing.Point(12, 46);
+            this.textBoxAnswers.Multiline = true;
+            this.textBoxAnswers.Name = "textBoxAnswers";
+            this.textBoxAnswers.Size = new System.Drawing.Size(355, 535);
+            this.textBoxAnswers.TabIndex = 11;
             // 
             // buttonNextQuestion
             // 
-            this.buttonNextQuestion.Location = new System.Drawing.Point(527, 528);
+            this.buttonNextQuestion.Location = new System.Drawing.Point(877, 524);
             this.buttonNextQuestion.Name = "buttonNextQuestion";
             this.buttonNextQuestion.Size = new System.Drawing.Size(125, 23);
             this.buttonNextQuestion.TabIndex = 35;
@@ -384,34 +154,10 @@ namespace POIProject
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1140, 656);
             this.Controls.Add(this.buttonNextQuestion);
-            this.Controls.Add(this.label18);
-            this.Controls.Add(this.label17);
-            this.Controls.Add(this.label16);
-            this.Controls.Add(this.label15);
-            this.Controls.Add(this.label14);
-            this.Controls.Add(this.label13);
-            this.Controls.Add(this.label12);
-            this.Controls.Add(this.label11);
-            this.Controls.Add(this.label10);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBoxTime5);
-            this.Controls.Add(this.textBoxAnswer5);
-            this.Controls.Add(this.textBoxTime4);
-            this.Controls.Add(this.textBoxAnswer4);
-            this.Controls.Add(this.textBoxAnswer3);
-            this.Controls.Add(this.textBoxTime3);
-            this.Controls.Add(this.textBoxTime2);
-            this.Controls.Add(this.textBoxAnswer2);
-            this.Controls.Add(this.textBoxTime1);
-            this.Controls.Add(this.textBoxAnswer1);
+            this.Controls.Add(this.textBoxAnswers);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.LabelA);
             this.Controls.Add(this.textBoxD);
             this.Controls.Add(this.textBoxC);
@@ -435,34 +181,10 @@ namespace POIProject
         private System.Windows.Forms.TextBox textBoxC;
         private System.Windows.Forms.TextBox textBoxD;
         private System.Windows.Forms.Label LabelA;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBoxAnswer1;
-        private System.Windows.Forms.TextBox textBoxTime1;
-        private System.Windows.Forms.TextBox textBoxAnswer2;
-        private System.Windows.Forms.TextBox textBoxTime2;
-        private System.Windows.Forms.TextBox textBoxTime3;
-        private System.Windows.Forms.TextBox textBoxAnswer3;
-        private System.Windows.Forms.TextBox textBoxAnswer4;
-        private System.Windows.Forms.TextBox textBoxTime4;
-        private System.Windows.Forms.TextBox textBoxAnswer5;
-        private System.Windows.Forms.TextBox textBoxTime5;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.Label label13;
-        private System.Windows.Forms.Label label14;
-        private System.Windows.Forms.Label label15;
-        private System.Windows.Forms.Label label16;
-        private System.Windows.Forms.Label label17;
-        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.TextBox textBoxAnswers;
         private System.Windows.Forms.Button buttonNextQuestion;
     }
 }
